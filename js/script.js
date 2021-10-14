4cd8f27cd3ea3050c102533e9b19adab939a83ff
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-14 17:28:26 +0200

    adds images to the image list container using javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//creo gli array 
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -22,12 +23,21 @@ const text = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
 
-
+//dichiaro una variabile che si riferisce al container della immagine attualmente visualizzata
 const currentImageContainer = document.querySelector (".current_image_container");
 
+
+//creo un ciclo che mette le immagini nell'html
 for(let i = 0; i < items.length; i++){
+    //assegno all'immagine corrente una variabile
     const currentImage = items[i];
 
-    
+    //inserisco all'interno del container dell'immagine (precedentemente dichiarato) il tag img, in modo da visualizzare le immagini
     currentImageContainer.innerHTML += `<img src="${currentImage}" alt="immagine #${currentImage}">`;
+}
+
+const imageListContainer = document.querySelector(".image_list_container")
+
+for (let i = 0; i < items.length; i++){
+    imageListContainer.innerHTML += `<img src="${items[i]}" alt="immagine #${items[i]}">`
 }
